384c4f6c3252a4564d84844c8f39e151d04aadc5
Davi Albino <negroalbino_@hotmail.com>
2023-05-03 19:03:48 -0300

    feat: study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,12 @@
-# Iniciando o projeto 
-
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None and type(target_time) != int:
+        return None
+
+    count_students = 0
+    for time in permanence_period:
+        if type(time[0]) != int or type(time[1]) != int:
+            return None
+        if time[0] <= target_time <= time[1]:
+            count_students += 1
+
+    return count_students
