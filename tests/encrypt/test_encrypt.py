da9f25d2cc06da7566415d074365c29e7024c717
Davi Albino <negroalbino_@hotmail.com>
2023-05-03 19:33:38 -0300

    test: encrypt message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(34, 34) == TypeError("tipo inválido para message")
+        encrypt_message('lrd', 'lad') == TypeError("tipo inválido para key")
+
+    assert encrypt_message('timberlake', 5) == "ebmit_ekalr"
+    assert encrypt_message('timberlake', 4) == "ekalre_bmit"
+    assert encrypt_message('lazer', 7) == "rezal"
