9131d1c302a4ad4ff7b1725dc2f19eb8a9e6b7ef
Davi Albino <negroalbino_@hotmail.com>
2023-05-03 21:45:29 -0300

    feat: palindromes recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == '':
+        return False
+    if len(word) == 1 or low_index >= high_index:
+        return True
+
+    if len(word) == 2 and word[low_index] == word[high_index]:
+        return True
+
+    if len(word) >= 3 and word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+    return False
